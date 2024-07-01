d64b51b9767b4d36945d2dc995a7f34a8f54b754
nightspite <n16h75p173@gmail.com>
2024-07-01 18:04:13 +0200

    feat: webhooks

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,53 @@
-import { Client, type OpenAPIConfig, OpenAPI } from '../client';
+import { Client, type OpenAPIConfig, OpenAPI, PostGetResponse } from '../client';
+import crypto from 'crypto';
+
+export type WebhookEventType = 'post.published';
+export type WebhookEvent = {
+  type: 'post.published';
+  data: PostGetResponse;
+};
+
+class Webhooks {
+  constructor() {}
+
+  /**
+   * Verify the signature of a webhook request
+   * @param rawBody - The raw body of the webhook request
+   * @param signature - The signature passed in the `x-signature` header
+   * @param secret - The webhook secret used to sign the request
+   * @returns Whether the signature is valid
+   */
+  private verifySignature(
+    rawBody: string,
+    signature: string,
+    secret: string,
+  ): boolean {
+    const body = typeof rawBody === 'string' ? rawBody : JSON.stringify(rawBody);
+
+    const sig = crypto.createHmac('sha256', secret).update(body).digest('hex');
+
+    return sig === signature;
+  }
+
+  /**
+   * Construct a webhook event from a request
+   * @param rawBody - The raw body of the webhook request
+   * @param signature - The signature passed in the `x-signature` header
+   * @param secret - The webhook secret used to sign the request
+   * @returns Parsed webhook event
+   */
+  public constructEvent(rawBody: string, signature: string, secret: string) {
+    if (!this.verifySignature(rawBody, signature, secret)) {
+      throw new Error('Invalid signature');
+    }
+
+    return JSON.parse(rawBody) as WebhookEvent;
+  }
+}
 
 export class Bundlesocial extends Client {
+  public webhooks: Webhooks;
+
   constructor(apiKey: string, options?: OpenAPIConfig) {
     super({
       ...options,
@@ -10,6 +57,8 @@ export class Bundlesocial extends Client {
         'x-api-key': apiKey,
       },
     });
+
+    this.webhooks = new Webhooks();
   }
 }
 

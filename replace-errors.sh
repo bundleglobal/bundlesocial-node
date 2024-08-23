e03d85b957160b16b4443909c1815569fab99f24
nightspite <n16h75p173@gmail.com>
2024-08-23 15:13:04 +0200

    fix: 429 error & reddit sr description

--- replace-errors.sh
+++ replace-errors.sh
@@ -6,11 +6,13 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   sed -i '' 's/401,/401: '"'"'401'"'"',/g' ./client/services.gen.ts
   sed -i '' 's/403,/403: '"'"'403'"'"',/g' ./client/services.gen.ts
   sed -i '' 's/404,/404: '"'"'404'"'"',/g' ./client/services.gen.ts
+  sed -i '' 's/429,/429: '"'"'429'"'"',/g' ./client/services.gen.ts
   sed -i '' 's/500/500: '"'"'500'"'"',/g' ./client/services.gen.ts
 else
   sed -i 's/400,/400: '"'"'400'"'"',/g' ./client/services.gen.ts
   sed -i 's/401,/401: '"'"'401'"'"',/g' ./client/services.gen.ts
   sed -i 's/403,/403: '"'"'403'"'"',/g' ./client/services.gen.ts
   sed -i 's/404,/404: '"'"'404'"'"',/g' ./client/services.gen.ts
+  sed -i 's/429,/429: '"'"'429'"'"',/g' ./client/services.gen.ts
   sed -i 's/500/500: '"'"'500'"'"',/g' ./client/services.gen.ts
 fi

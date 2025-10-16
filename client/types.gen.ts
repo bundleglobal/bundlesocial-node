85da124f91358b0bd8a4d4158e1e318a75c5948d
nightspite <n16h75p173@gmail.com>
2025-10-16 09:40:06 +0200

    feat: add showModalOnConnectSuccess option for create portal link

--- client/types.gen.ts
+++ client/types.gen.ts
@@ -642,6 +642,7 @@ export type SocialAccountCreatePortalLinkData = {
         hideUserLogo?: boolean;
         hideUserName?: boolean;
         hideLanguageSwitcher?: boolean;
+        showModalOnConnectSuccess?: boolean;
         language?: 'en' | 'pl' | 'fr' | 'hi' | 'sv';
     };
 };

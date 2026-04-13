9be2eb54a481007bcefe7426feea5c2f38db2583
nightspite <n16h75p173@gmail.com>
2026-04-13 22:37:53 +0200

    feat: comments

--- client/Client.ts
+++ client/Client.ts
@@ -5,28 +5,15 @@ import { FetchHttpRequest } from './core/FetchHttpRequest';
 
 import { AnalyticsService } from './services.gen';
 import { AppService } from './services.gen';
-import { BlueskyService } from './services.gen';
 import { CommentService } from './services.gen';
-import { DiscordService } from './services.gen';
-import { FacebookService } from './services.gen';
-import { GoogleBusinessService } from './services.gen';
-import { InstagramService } from './services.gen';
-import { LinkedinService } from './services.gen';
-import { MastodonService } from './services.gen';
 import { MiscService } from './services.gen';
 import { OrganizationService } from './services.gen';
-import { PinterestService } from './services.gen';
 import { PostService } from './services.gen';
 import { PostCsvService } from './services.gen';
 import { PostImportService } from './services.gen';
-import { RedditService } from './services.gen';
-import { SlackService } from './services.gen';
 import { SocialAccountService } from './services.gen';
 import { TeamService } from './services.gen';
-import { TiktokService } from './services.gen';
-import { TwitterService } from './services.gen';
 import { UploadService } from './services.gen';
-import { YoutubeService } from './services.gen';
 
 type HttpRequestConstructor = new (config: OpenAPIConfig) => BaseHttpRequest;
 
@@ -34,28 +21,15 @@ export class Client {
 
 	public readonly analytics: AnalyticsService;
 	public readonly app: AppService;
-	public readonly bluesky: BlueskyService;
 	public readonly comment: CommentService;
-	public readonly discord: DiscordService;
-	public readonly facebook: FacebookService;
-	public readonly googleBusiness: GoogleBusinessService;
-	public readonly instagram: InstagramService;
-	public readonly linkedin: LinkedinService;
-	public readonly mastodon: MastodonService;
 	public readonly misc: MiscService;
 	public readonly organization: OrganizationService;
-	public readonly pinterest: PinterestService;
 	public readonly post: PostService;
 	public readonly postCsv: PostCsvService;
 	public readonly postImport: PostImportService;
-	public readonly reddit: RedditService;
-	public readonly slack: SlackService;
 	public readonly socialAccount: SocialAccountService;
 	public readonly team: TeamService;
-	public readonly tiktok: TiktokService;
-	public readonly twitter: TwitterService;
 	public readonly upload: UploadService;
-	public readonly youtube: YoutubeService;
 
 	public readonly request: BaseHttpRequest;
 
@@ -78,27 +52,14 @@ export class Client {
 
 		this.analytics = new AnalyticsService(this.request);
 		this.app = new AppService(this.request);
-		this.bluesky = new BlueskyService(this.request);
 		this.comment = new CommentService(this.request);
-		this.discord = new DiscordService(this.request);
-		this.facebook = new FacebookService(this.request);
-		this.googleBusiness = new GoogleBusinessService(this.request);
-		this.instagram = new InstagramService(this.request);
-		this.linkedin = new LinkedinService(this.request);
-		this.mastodon = new MastodonService(this.request);
 		this.misc = new MiscService(this.request);
 		this.organization = new OrganizationService(this.request);
-		this.pinterest = new PinterestService(this.request);
 		this.post = new PostService(this.request);
 		this.postCsv = new PostCsvService(this.request);
 		this.postImport = new PostImportService(this.request);
-		this.reddit = new RedditService(this.request);
-		this.slack = new SlackService(this.request);
 		this.socialAccount = new SocialAccountService(this.request);
 		this.team = new TeamService(this.request);
-		this.tiktok = new TiktokService(this.request);
-		this.twitter = new TwitterService(this.request);
 		this.upload = new UploadService(this.request);
-		this.youtube = new YoutubeService(this.request);
 	}
 }

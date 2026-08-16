907ec03ebbe1e6f160a537f73ace931818dee299
nightspite <n16h75p173@gmail.com>
2026-08-16 18:46:55 +0200

    chore: update replace-errors.sh [skip ci]

--- replace-errors.sh
+++ replace-errors.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-# replace all errors in the file ./client/services.gen.ts
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i '' 's/400,/400: '"'"'400'"'"',/g' ./client/services.gen.ts
-  sed -i '' 's/401,/401: '"'"'401'"'"',/g' ./client/services.gen.ts
-  sed -i '' 's/403,/403: '"'"'403'"'"',/g' ./client/services.gen.ts
-  sed -i '' 's/404,/404: '"'"'404'"'"',/g' ./client/services.gen.ts
-  sed -i '' 's/409,/409: '"'"'409'"'"',/g' ./client/services.gen.ts
-  sed -i '' 's/422,/422: '"'"'422'"'"',/g' ./client/services.gen.ts
-  sed -i '' 's/429,/429: '"'"'429'"'"',/g' ./client/services.gen.ts
-  sed -i '' 's/500/500: '"'"'500'"'"',/g' ./client/services.gen.ts
-  sed -i '' 's/502/502: '"'"'502'"'"',/g' ./client/services.gen.ts
+  sed -i '' -E "s/^([[:space:]]*)([0-9]{3}),?$/\1\2: '\2',/" ./client/services.gen.ts
 else
-  sed -i 's/400,/400: '"'"'400'"'"',/g' ./client/services.gen.ts
-  sed -i 's/401,/401: '"'"'401'"'"',/g' ./client/services.gen.ts
-  sed -i 's/403,/403: '"'"'403'"'"',/g' ./client/services.gen.ts
-  sed -i 's/404,/404: '"'"'404'"'"',/g' ./client/services.gen.ts
-  sed -i 's/409,/409: '"'"'409'"'"',/g' ./client/services.gen.ts
-  sed -i 's/422,/422: '"'"'422'"'"',/g' ./client/services.gen.ts
-  sed -i 's/429,/429: '"'"'429'"'"',/g' ./client/services.gen.ts
-  sed -i 's/500/500: '"'"'500'"'"',/g' ./client/services.gen.ts
-  sed -i 's/502/502: '"'"'502'"'"',/g' ./client/services.gen.ts
+  sed -i -E "s/^([[:space:]]*)([0-9]{3}),?$/\1\2: '\2',/" ./client/services.gen.ts
 fi
